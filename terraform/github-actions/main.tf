f46d5c5107c7315bf1651782d97a58feb2886e56
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-10-14 00:45:24 +0900

    feat: GitHub Actions用のインフラを準備

---
+++ terraform/github-actions/main.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  # https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
+}
+
+# https://docs.github.com/ja/actions/security-for-github-actions/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
+resource "aws_iam_role" "github_actions" {
+  name = "admin-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.repo_name}:*"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.github_actions.name
+}
+
+output "role_arn" {
+  value       = aws_iam_role.github_actions.arn
+  description = "The ARN of the IAM role for GitHub Actions."
+}
+
+output "provider_url" {
+  value = aws_iam_openid_connect_provider.github_actions.url
+}
---
+++ terraform/github-actions/variables.tf
@@ -0,0 +1,11 @@
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+  default     = "shimopino-org"
+}
+
+variable "repo_name" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "*"
+}
