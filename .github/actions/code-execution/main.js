4e38e44802650de4d8a19fb1aabcff427bdcb53a
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-10-14 13:46:18 +0900

    fix: コード実行用の設定を追加

--- .github/actions/code-execution/main.js
+++ .github/actions/code-execution/main.js
@@ -1,20 +1,19 @@
-// const core = require("@actions/core");
-// const github = require("@actions/github");
+module.exports = async ({ github, context, core }) => {
+  try {
+    const environment = core.getInput("environment");
+    const sampleValue = core.getInput("sample-value");
+    console.log(`Hello ${environment}!`);
+    console.log(`Hello ${sampleValue}!`);
 
-try {
-  const environment = core.getInput("environment");
-  const sampleValue = core.getInput("sample-value");
-  console.log(`Hello ${environment}!`);
-  console.log(`Hello ${sampleValue}!`);
+    // コンテキスト情報の取得
+    const context = github.context;
+    console.log(`Event Name: ${context.eventName}`);
+    console.log(`Workflow: ${context.workflow}`);
+    console.log(`Actor: ${context.actor}`);
 
-  // コンテキスト情報の取得
-  const context = github.context;
-  console.log(`Event Name: ${context.eventName}`);
-  console.log(`Workflow: ${context.workflow}`);
-  console.log(`Actor: ${context.actor}`);
-
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
