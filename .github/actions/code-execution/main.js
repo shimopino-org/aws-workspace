515ad906ecef31f401eee3725a2dd2cc4063a359
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-10-14 13:38:51 +0900

    fix: 挙動確認用

--- .github/actions/code-execution/main.js
+++ .github/actions/code-execution/main.js
@@ -1,5 +1,5 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+// const core = require("@actions/core");
+// const github = require("@actions/github");
 
 try {
   const environment = core.getInput("environment");
