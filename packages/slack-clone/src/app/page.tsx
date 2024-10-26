27660686f55ff1b7be2414cf1b2cfc3ab973eee2
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-10-26 14:30:18 +0900

    fix: ユーザーアイコンのドロップダウンメニューを追加

--- packages/slack-clone/src/app/page.tsx
+++ packages/slack-clone/src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-	const { signOut } = useAuthActions();
-
 	return (
 		<div className="h-full">
-			Home Page
-			<Button disabled={false} onClick={() => signOut()}>
-				Sign Out
-			</Button>
+			<UserButton />
 		</div>
 	);
 }
